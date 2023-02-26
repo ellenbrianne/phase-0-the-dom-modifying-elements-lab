3180f868f6ec7d96c3c9343cec18f6975e85cd90
Ellen Bennett <ellenbennett@Ellens-MacBook-Air.local>
2023-02-25 20:11:23 -0700

    passing lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+const main = document.querySelector('#main');
+
+main.remove();
+
+let newHeader = document.createElement('H1');
+
+newHeader.id = 'victory';
+
+newHeader.textContent = 'ELLEN is the champion';
